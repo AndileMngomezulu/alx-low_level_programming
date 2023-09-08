501cc6c11b8c4d7e8c14e05a37ea5ddec7f7b11a
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-08 03:09:49 -0700

    Preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function that prints exactly
+ *
+ * Return: 0 if it succeed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0)
