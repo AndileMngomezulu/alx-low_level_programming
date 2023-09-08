a3f0278aa1c4a89903549075e1025a530e395642
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-08 03:40:32 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - this is the main function 
- *
- * Return:  always 0
- */
+  * main - it is the main function
+  *
+  *Return: it always should be 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building multilingual puzzle");
