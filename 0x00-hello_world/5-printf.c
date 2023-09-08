1665c20ddb5bc14f9fd69e8a633d372273438e70
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-08 04:12:01 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - the main function that prints exactly
  *
- * Return: 0 when it succeed
+ * Return: 0 when it is successful
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+}
