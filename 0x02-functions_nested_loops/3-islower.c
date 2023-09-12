ba0895418b694b1d4ea6582f23b5f1628fc1bcfc
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-12 06:11:16 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _islower - checks for smaller character
+  * @c: to be checked parameter
+  * Return: 1 or 0
+  */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'a')
+		return (1);
+	else
+		return (0);
+}
