fc0970a9ea10f333191a858956168de32d19ba4f
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-12 05:15:41 -0700

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
-  * main - check the code
+  * print_alphabet - print the alphabets in lower case
   *
   * Return: always 0
   */
 void print_alphabet(void)
 {
-
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		_putchar(c);
-	}
 	_putchar('\n');
+
 }
