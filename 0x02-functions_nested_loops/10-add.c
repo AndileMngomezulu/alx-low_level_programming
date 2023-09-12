3a3af2686c45be9f8cc5e4988a6ef302f3534454
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-12 10:18:17 -0700

    Adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * add - fuction that adds up to two numbers
+  * @y: parameter 1
+  * @z: parameter 2
+  * Return: y + z
+  */
+
+int add(int y, int z)
+{
+	return (y + z);
+}
