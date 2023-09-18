2203837765f077a5b799e2ad23f10b128c549a5d
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-18 10:34:45 -0700

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
   * _puts - prints a string
   * @str: string to print
@@ -11,7 +11,7 @@ void _puts(char *str)
 
 	for (i = 0 ; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
