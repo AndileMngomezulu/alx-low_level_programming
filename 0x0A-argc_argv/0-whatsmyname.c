0703353f79845f1e9e6ac622e435513d1972cf1a
AndileMngomezulu <andymdlalose1@gmail.com>
2023-09-29 06:00:11 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - it is the main function
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: void
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
